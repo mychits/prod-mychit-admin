cae0c60f47683299f87819f6878c517196c3ff74
grash2024 <grash2024@gmail.com>
2025-02-03 15:05:07 +0530

    converted nonscrollable modal to scrollable modal

--- src/components/modals/Modal.jsx
+++ src/components/modals/Modal.jsx
@@ -9,20 +9,23 @@ const Modal = ({ isVisible, onClose, children }) => {
 
   return (
     <>
-      <div
-        className="fixed inset-0 bg-black bg-opacity-25 backdrop-blur-sm flex justify-center items-center z-10"
-        onClick={handleClose}
-      >
-        <div className="w-[600px] flex flex-col relative">
-          <button
-            className="text-gray-700 text-2xl absolute top-2 right-2 hover:bg-gray-100"
-            onClick={() => onClose()}
-          >
-           <IoMdClose />
-          </button>
-          <div className="bg-white p-2 rounded">{children}</div>
-        </div>
-      </div>
+  <div 
+  className="fixed inset-0 bg-black bg-opacity-25 backdrop-blur-sm flex justify-center items-center z-10"
+  onClick={handleClose}
+>
+  <div className="w-[600px] max-h-[calc(100vh-5rem)] flex flex-col relative overflow-hidden">
+    <button
+      className="text-gray-700 text-2xl absolute top-2 right-2 hover:bg-gray-100 z-20"
+      onClick={() => onClose()}
+    >
+      <IoMdClose />
+    </button>
+    <div className="bg-white p-2 rounded overflow-y-auto 
+      [&::-webkit-scrollbar]:hidden [scrollbar-width:none]">
+      {children}
+    </div>
+  </div>
+</div>
     </>
   );
 };
