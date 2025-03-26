752722cb8e68da345d78f42926df1ac819682db5
Anup_developer <anup42564@gmail.com>
2025-03-26 13:48:07 +0530

    mar 26

--- src/data/Url.js
+++ src/data/Url.js
@@ -1,3 +1,3 @@
-// const url = "http://localhost:3000/api";
-const url = "http://13.51.185.18:3000/api";
+//const url = "http://localhost:3000/api";
+const url = "http://51.21.197.152:3000/api";
 export default url;
--- src/instance/TokenInstance.js
+++ src/instance/TokenInstance.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 const token = localStorage.getItem("token");
 const api = axios.create({
-  // baseURL: "http://13.51.185.18:3000/api",
-  baseURL: "http://localhost:3000/api",
+  baseURL: "http://51.21.197.152:3000/api",
+  // baseURL: "http://localhost:3000/api",
   headers: {
     Authorization: `Bearer ${token}`,
   },
